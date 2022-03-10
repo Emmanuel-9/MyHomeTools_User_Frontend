c3ec282121bc7addda344680d9ee404fb0a23c0a
IshaqKassam <ishaq.kassam@gmail.com>
2022-03-10 21:03:34 +0300

    added the resource

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Card from "./components/Card"
 import Register from "./components/Register"
+import Login from "./components/Login"
 
 
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Card/>} />
           <Route path="/register" element={<Register/>} />
+          <Route path="/login" element={<Login/>} />
         
           </Routes>
    
