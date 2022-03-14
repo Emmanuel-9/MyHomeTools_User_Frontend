1ece99f64c10563f7b120dd559604e47b62fcf59
Caleno-R <calenorose@gmail.com>
2022-03-14 13:29:08 +0300

    fixed the layout

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,4 @@
 import styled from "styled-components"
-import Add from "@mui/icons-material/Add"
 import React, { useState } from "react"
 
 function Cart() {
@@ -9,7 +8,7 @@ function Cart() {
     setCount(count + 1)
   }
   const subtract = () => {
-    if (count != 0) {
+    if (count !== 0) {
       setCount(count - 1)
     }
   }
@@ -18,82 +17,87 @@ function Cart() {
     <Container>
       <Wrapper>
         <Title>Shopping Cart</Title>
-        <TopButton type="filled">Continue Shopping</TopButton>
+        <TopButton type="filled">Continue Shopping </TopButton>
+
         <Bottom>
           <Info>
             <Product>
               <ProductDetail>
-                <img src="/images/juicer.png" alt="tv" />
+                <img src="/images/smart-tv.png" alt="tv" />
                 <Details>
-                  <ProductName>Blender</ProductName>
+                  <ProductName>LED Backlit Smart TV</ProductName>
                 </Details>
               </ProductDetail>
             </Product>
           </Info>
           <Count>
-            <p id="add" onClick={add}>
-              +
-            </p>
-            <p>{count}</p>
-
             <p id="subtract" onClick={subtract}>
               -
+            </p>
+            <p>{count}</p>
+            <p id="add" onClick={add}>
+              +
             </p>
           </Count>
           <ProductPrice>Ksh 130,000</ProductPrice>
         </Bottom>
+
         <Hr />
+
         <Bottom>
           <Info>
             <Product>
               <ProductDetail>
-                <img src="/images/juicer.png" alt="tv" />
+                <img src="/images/dishwasher.png" alt="dishwasher" />
                 <Details>
-                  <ProductName>Blender</ProductName>
+                  <ProductName>Automatic Dishwasher</ProductName>
                 </Details>
               </ProductDetail>
             </Product>
           </Info>
           <Count>
-            <p id="add" onClick={add}>
-              +
-            </p>
-            <p>{count}</p>
-
             <p id="subtract" onClick={subtract}>
               -
             </p>
+            <p>{count}</p>
+            <p id="add" onClick={add}>
+              +
+            </p>
           </Count>
-          <ProductPrice>Ksh 130,000</ProductPrice>
-          <Hr />
+          <ProductPrice>Ksh 160,000</ProductPrice>
         </Bottom>
+
         <Hr />
+
         <Bottom>
           <Info>
             <Product>
               <ProductDetail>
-                <img src="/images/juicer.png" alt="tv" />
+                <img src="/images/deep-frier.png" alt="deep-frier" />
                 <Details>
-                  <ProductName>Blender</ProductName>
+                  <ProductName>Deep Frier</ProductName>
                 </Details>
               </ProductDetail>
             </Product>
           </Info>
           <Count>
-            <p id="add" onClick={add}>
-              +
-            </p>
-            <p>{count}</p>
-
             <p id="subtract" onClick={subtract}>
               -
             </p>
+            <p>{count}</p>
+            <p id="add" onClick={add}>
+              +
+            </p>
           </Count>
-          <ProductPrice>Ksh 130,000</ProductPrice>
-          <Hr />
+          <ProductPrice>Ksh 60,000</ProductPrice>
         </Bottom>
+       <Hr />
       </Wrapper>
-      <BottomButton type="filled">Checkout</BottomButton> 
+
+      <Total>
+        <p className = "total" >Sub-Total: Ksh - 350,000</p>
+      </Total>
+      <BottomButton type="filled">Checkout</BottomButton>
     </Container>
   )
 }
@@ -101,23 +105,23 @@ export default Cart
 
 const Container = styled.div`
   background: rgba(196, 196, 196, 0.2);
-  height: 100vh;
+  height: 120vh;
 `
 const Wrapper = styled.div`
-  padding: 30px;
-  /* border: 1px solid black; */
+  padding: 20px;
   align-items: center;
 `
 const Title = styled.div`
-  font-weight: 400;
+  /* font-weight: 1500px; */
+  font-style: bold;
   text-align: center;
-  font-size: 25px;
+  font-size: 20px;
   line-height: 29.3px;
-  font-style: normal;
+  
 `
 const TopButton = styled.button`
   padding: 10px;
-  font-weight: 600;
+  font-weight: 300;
   cursor: pointer;
   margin: 0 0 20px 0;
   border-radius: 35px;
@@ -147,8 +151,6 @@ const Product = styled.div`
   justify-content: space-between;
 `
 const ProductDetail = styled.div`
-  /* flex: 2; */
-
   img {
     width: 100px;
   }
@@ -164,11 +166,9 @@ const Details = styled.div`
 const ProductName = styled.span``
 
 const ProductPrice = styled.div`
-  /* width: 50px; */
   position: relative;
   right: 0;
   align-items: right;
-  /* border: 1px solid red; */
 `
 
 const Count = styled.div`
@@ -177,6 +177,7 @@ const Count = styled.div`
   align-items: center;
   width: 100px;
   border: 1px solid grey;
+  background-color: lightgrey;
   border-radius: 10px;
   height: 100%;
 
@@ -201,25 +202,39 @@ const Count = styled.div`
   }
 `
 const Hr = styled.hr`
-  background-color: #eee;
+  background-color: #6c6a6a;
   border: none;
-  height: 4px;
+  height: 2px;
 `
+
+const Total = styled.div`
+  border: 10 px solid red;
+  position: absolute;
+  right: 0;
+  margin:30px 20px 0 0;
+  
+  .total{
+    
+    color: green;
+    font-size: 25px;
+  }
+`
+
 const BottomButton = styled.button`
   padding: 10px;
-  font-weight: 600;
+  font-weight: 300;
   position: absolute;
-  left: 500px;
+  right: 0;
   cursor: pointer;
-  margin: 0 0 20px 0;
+  margin: 80px 20px 0px 0;
   border-radius: 35px;
   border: ${(props) => props.type === "filled" && "none"};
   background-color: ${(props) => props.type === "filled" && "black"};
-  color: ${(props) => props.type === "filled" && "white"}; 
+  color: ${(props) => props.type === "filled" && "white"};
   transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
   &:hover {
     box-shadow: 0 15px 15px rgba(0, 0, 0, 0.16);
     transform: translate(0, -5px);
-  }  
-  
-  `
+  }
+`
+// @media only screen and min-width: 786px;
