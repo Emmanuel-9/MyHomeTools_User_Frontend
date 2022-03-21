812de0f3dc352f5ee36ea44df2a4e116623baedf
Caleno-R <calenorose@gmail.com>
2022-03-21 16:17:31 +0300

    renamed cart component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Register from "./components/Register"
 import Login from "./components/Login"
-import Cart from "./components/Cart"
+import CartCheckout from "./components/CartCheckout"
 import Products from "./Products"
 import ProductDetail from "./ProductDetail"
 import Home from "./Home"
@@ -18,14 +18,12 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          {/* <Route path="/" element={<Card />} /> */}
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/products/" element={<Products />} />
 
           <Route path="/products/:productId" element={<ProductDetail />} />
-          <Route path="/cart" element={<Cart />} />
-          {/* <Route path="/details" element={<CheckoutDetails />} /> */}
+          <Route path="/cart" element={<CartCheckout />} />
 
           <Route path="/" element={<Home />} />
         </Routes>
