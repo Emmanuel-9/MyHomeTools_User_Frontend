c9fdb762da18455398532806ef75ed6c6cdc46e7
IshaqKassam <ishaq.kassam@gmail.com>
2022-04-01 09:51:38 +0300

    cart total

--- src/components/CartCheckout.js
+++ src/components/CartCheckout.js
@@ -71,10 +71,10 @@ function CartCheckout() {
 						}
 					})
 				}
-				//
+
 				mypromise()
 					.then((res) => {
-						// console.log("my promise is", mypromise())
+						console.log("my promise is", mypromise())
 						setGrandTotal(res().reduce((a, b) => a + b, 0))
 					})
 					.then(() => {
@@ -86,16 +86,16 @@ function CartCheckout() {
 			})
 	}, [reload])
 
-  console.log("cart is", cart)
+	console.log("cart is", cart)
 
-  const add = () => {
-    setCount(count + 1)
-  }
-  const subtract = () => {
-    if (count !== 0) {
-      setCount(count - 1)
-    }
-  }
+	const add = () => {
+		setCount(count + 1)
+	}
+	const subtract = () => {
+		if (count !== 0) {
+			setCount(count - 1)
+		}
+	}
 
 	const handleRemove = (product) => (e) => {
 		//modify the current stateful list with a filter function
@@ -163,176 +163,176 @@ function CartCheckout() {
 					</Total>
 				</Wrapper>
 
-        <CheckoutDetails />
-      </Container>
-    )
-  } else {
-    return <p>Oops, Nothing is cart</p>
-  }
+				<CheckoutDetails />
+			</Container>
+		)
+	} else {
+		return <p>Oops, Nothing is cart</p>
+	}
 }
 
 export default CartCheckout
 
 const Container = styled.div`
-  background: rgba(196, 196, 196, 0.2);
-  width: 100%;
-  margin: 70px 0 0 0;
-  display: flex;
-  flex-wrap: wrap;
+	background: rgba(196, 196, 196, 0.2);
+	width: 100%;
+	margin: 70px 0 0 0;
+	display: flex;
+	flex-wrap: wrap;
 
-  @media only screen and (min-width: 786px) {
-    margin: 0;
-  }
+	@media only screen and (min-width: 786px) {
+		margin: 0;
+	}
 `
 
 const Wrapper = styled.div`
-  padding: 20px;
-  align-items: center;
-  height: 50vh;
-  overflow-y: scroll;
-  ::-webkit-scrollbar {
-    display: none;
-  }
+	padding: 20px;
+	align-items: center;
+	height: 50vh;
+	overflow-y: scroll;
+	::-webkit-scrollbar {
+		display: none;
+	}
 
-  @media only screen and (min-width: 768px) {
-    width: 50%;
-    height: 90vh;
-    overflow-y: scroll;
-    ::-webkit-scrollbar {
-      display: none;
-    }
-  }
+	@media only screen and (min-width: 768px) {
+		width: 50%;
+		height: 90vh;
+		overflow-y: scroll;
+		::-webkit-scrollbar {
+			display: none;
+		}
+	}
 `
 const Title = styled.div`
-  font-style: bold;
-  text-align: center;
-  font-size: 20px;
-  line-height: 29.3px;
+	font-style: bold;
+	text-align: center;
+	font-size: 20px;
+	line-height: 29.3px;
 `
 const TopButton = styled.button`
-  padding: 10px;
-  font-weight: 300;
-  cursor: pointer;
-  margin: 0 0 20px 0;
-  border-radius: 35px;
-  border: none;
-  background-color: black;
-  color: white;
-  transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
-  &:hover {
-    box-shadow: 0 15px 15px rgba(0, 0, 0, 0.16);
-    transform: translate(0, -5px);
-  }
+	padding: 10px;
+	font-weight: 300;
+	cursor: pointer;
+	margin: 0 0 20px 0;
+	border-radius: 35px;
+	border: none;
+	background-color: black;
+	color: white;
+	transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
+	&:hover {
+		box-shadow: 0 15px 15px rgba(0, 0, 0, 0.16);
+		transform: translate(0, -5px);
+	}
 
-  @media only screen and (min-width: 786px) {
-    border: none;
-    background-color: green;
-    color: white;
-  }
+	@media only screen and (min-width: 786px) {
+		border: none;
+		background-color: green;
+		color: white;
+	}
 `
 const Bottom = styled.div`
-  /* height: 100vh; */
-  justify-content: space-around;
-  align-items: center;
+	/* height: 100vh; */
+	justify-content: space-around;
+	align-items: center;
 
-  /* p {
+	/* p {
 		height: 100px;
 	} */
 `
 const Box = styled.div`
-  border: 1px solid lightgrey;
-  margin: 10px 0;
-  padding: 10px;
+	border: 1px solid lightgrey;
+	margin: 10px 0;
+	padding: 10px;
 `
 const Product = styled.div`
-  display: flex;
-  /* width: 50%; */
-  margin: 10px 0;
-  justify-content: space-between;
+	display: flex;
+	/* width: 50%; */
+	margin: 10px 0;
+	justify-content: space-between;
 `
 const ProductDetail = styled.div`
-  padding: 0;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+	padding: 0;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
 
-  img {
-    width: 50%;
-    padding: 0;
-  }
+	img {
+		width: 50%;
+		padding: 0;
+	}
 
-  @media only screen and (min-width: 786px) {
-    margin: 0 0 0 20px;
-  }
+	@media only screen and (min-width: 786px) {
+		margin: 0 0 0 20px;
+	}
 `
 const ProductName = styled.span`
-  width: 100%;
-  margin: 15px;
+	width: 100%;
+	margin: 15px;
 `
 const ProductPrice = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 10px auto;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin: 10px auto;
 `
 const Count = styled.div`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  padding: 10px;
-  border: 1px solid grey;
-  margin: 0 auto;
-  background-color: lightgrey;
-  border-radius: 10px;
-  height: 100%;
-  width: 50%;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	padding: 10px;
+	border: 1px solid grey;
+	margin: 0 auto;
+	background-color: lightgrey;
+	border-radius: 10px;
+	height: 100%;
+	width: 50%;
 
-  #add {
-    padding: 5px;
-    font-size: 20px;
-    font-weight: bold;
+	#add {
+		padding: 5px;
+		font-size: 20px;
+		font-weight: bold;
 
-    :hover {
-      background: lightgrey;
-      cursor: pointer;
-      border-radius: 30%;
-    }
-  }
+		:hover {
+			background: lightgrey;
+			cursor: pointer;
+			border-radius: 30%;
+		}
+	}
 
-  #subtract {
-    padding: 5px;
-    font-size: 20px;
-    font-weight: bold;
+	#subtract {
+		padding: 5px;
+		font-size: 20px;
+		font-weight: bold;
 
-    :hover {
-      background: lightgrey;
-      cursor: pointer;
-      border-radius: 30%;
-    }
-  }
+		:hover {
+			background: lightgrey;
+			cursor: pointer;
+			border-radius: 30%;
+		}
+	}
 `
 const Hr = styled.hr`
-  background-color: #6c6a6a;
-  border: none;
-  height: 2px;
+	background-color: #6c6a6a;
+	border: none;
+	height: 2px;
 `
 
 const Total = styled.div`
-  border: 10 px solid red;
-  margin: 10px 20px 0 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	border: 10 px solid red;
+	margin: 10px 20px 0 0;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 
-  .total {
-    font-weight: bold;
-    color: green;
-    font-size: 20px;
-  }
+	.total {
+		font-weight: bold;
+		color: green;
+		font-size: 20px;
+	}
 
-  @media only screen and (min-width: 786px) {
-    margin: 20px 20px 0px 0;
-    left: 480px;
-  }
+	@media only screen and (min-width: 786px) {
+		margin: 20px 20px 0px 0;
+		left: 480px;
+	}
 `
